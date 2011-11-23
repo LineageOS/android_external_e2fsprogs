4410f59b2b79ed171e7c331e167949c6b8663479
Austen Dicken <cvpcsm@gmail.com>
2011-11-23 01:03:46 -0600

    build static e2fsprogs
    change makefies to build static mke2fs and tune2fs and place them in
    $(PRODUCT_OUT)/utilities
    
    Change-Id: I0139b3dcfe64a4031db7719ddd71120d174d1990
    
    static e2fsck, tune2fs, and mke2fs for recovery
    
    Change-Id: I8725cc296be8967265261e92da743c149e9ec45b
    
    resize: allow static build
    
    Change-Id: Id5dabaf87de775bb7461f83a9e7ae9ca754a1635
    
    e2fsprogs: Clean up makefiles
    
     * Remove duplicate modules
    
    Change-Id: If68c5685a76943bc6bfdcb630c15c2f9ac59effd
    (cherry picked from commit 2239e5be5ed8df24a552d1d83b74beb35652093d)

--- resize/Android.mk
+++ resize/Android.mk
@@ -32,3 +32,31 @@ LOCAL_MODULE_TAGS := optional
 LOCAL_MODULE_PATH := $(PRODUCT_OUT)/install/bin
 LOCAL_FORCE_STATIC_EXECUTABLE := true
 include $(BUILD_EXECUTABLE)
+<<<<<<< HEAD   (b2b145 Mark libext2{-headers,_blkid,_uuid} as vendor available)
+=======
+
+include $(CLEAR_VARS)
+
+LOCAL_SRC_FILES := $(resize2fs_src_files)
+LOCAL_C_INCLUDES := $(resize2fs_c_includes)
+LOCAL_CFLAGS := $(resize2fs_cflags)
+LOCAL_STATIC_LIBRARIES := $(resize2fs_shared_libraries)
+LOCAL_STATIC_LIBRARIES += $(resize2fs_system_shared_libraries)
+LOCAL_MODULE := resize2fs_static
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE_PATH := $(PRODUCT_OUT)/install/bin
+LOCAL_FORCE_STATIC_EXECUTABLE := true
+include $(BUILD_EXECUTABLE)
+
+include $(CLEAR_VARS)
+
+LOCAL_SRC_FILES := $(resize2fs_src_files)
+LOCAL_C_INCLUDES := $(resize2fs_c_includes)
+LOCAL_CFLAGS := $(resize2fs_cflags)
+LOCAL_SHARED_LIBRARIES := $(addsuffix _host, $(resize2fs_shared_libraries))
+LOCAL_MODULE := resize2fs_host
+LOCAL_MODULE_STEM := resize2fs
+LOCAL_MODULE_TAGS := optional
+
+include $(BUILD_HOST_EXECUTABLE)
+>>>>>>> CHANGE (2239e5 build static e2fsprogs)
